cbaefa395ff682d6b64014e6f0e6bdaca89def18
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-07-16 09:46:25 -0400

    first test

--- public/app.js
+++ public/app.js
@@ -13,11 +13,12 @@ document.body.appendChild(div)
 div.appendChild(document.createTextNode('Initializing...'))
 
 function render() {
-  const shouldShow = flagsmith.getValue('feature_a')
+  const shouldShow = flagsmith.hasFeature('feature_a')
   const label = (shouldShow ? 'Showing' : 'Not showing') + ' feature A'
   div.replaceChild(document.createTextNode(label), div.firstChild)
 }
 
+// https://docs.flagsmith.com/clients/javascript/
 flagsmith.init({
   // comes from the Flagsmith project settings
   environmentID: 'gxzgHaQ84gijocUvctHJFb',
